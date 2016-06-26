5ff3fa9406316f263087f9619dd16b7aec54584b
Melanie Archer <job@twobanjos.com>
2016-06-25 17:50:30 -0700

    placeholder index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('TBD');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
